c8e1d52f638b1a31fd22981c9cfe068de26a6598
Saeed Setayesh <iamsaeedsetayesh@gmail.com>
2026-07-11 14:06:38 -0600

    fixed: change the database mingration

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,3 +2,9 @@ import NextAuth from "next-auth";
 import { authConfig } from "@/lib/auth.config";
 
 export default NextAuth(authConfig).auth;
+
+export const config = {
+  matcher: [
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp|ico|css|js|woff|woff2)$).*)",
+  ],
+};
